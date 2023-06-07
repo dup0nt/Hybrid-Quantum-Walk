fc67fa740c4e2c8dc4260ea181b54b9fdaee681f
dup0nt <diogo.mfg00@gmail.com>
2023-06-07 11:41:54 +0100

    upgrade

--- Job_data/compiler.py
+++ Job_data/compiler.py
@@ -1,23 +1,70 @@
 import pandas as pd
+from matplotlib import pyplot as plt
 
-cache = {}
 
-def read_file(file_name):
-    if file_name not in cache:
-        with open(file_name, "r") as file:
-            cache[file_name] = file.readlines()
-    return cache[file_name]
+file_name = "/Users/diogogoncalves/Documents/GitProjects/Thesis/Dirac Quantum Walk/Job_data/sacctoutput.csv"
+df = pd.read_csv(file_name)
+df = df[(~df['State'].str.contains('CANCELLED|FAILED')) & (~df['JobID'].str.contains('.extern'))]
 
-file_name = "/Users/diogogoncalves/Documents/GitProjects/Thesis/Dirac Quantum Walk/Job_data/beta_jobs_results.txt"
+df['ShiftedMaxRSS'] = df['MaxRSS'].shift(-1)
 
-data = read_file(file_name)
+df.loc[~df['JobName'].str.contains('.batch'), 'MaxRSS'] = df.loc[~df['JobName'].str.contains('.batch'), 'ShiftedMaxRSS']
 
-column_names = data[0].strip().split()  # Apply .strip() and .split() to the column names
-data_rows = data[2:]
+df.loc[df['JobName'].str.contains('.batch'), 'MaxRSS'] = None
+df = df.drop(columns=['ShiftedMaxRSS'])
+df = df[(~df['JobID'].str.contains('.batch'))]
 
-# Apply .strip() and .split() to each row in data_rows
-processed_rows = [row.split() for row in data_rows]
+df['qubits'] = df['JobName'].str.extract(r'Q(\d+)S')
+df['steps'] = df['JobName'].str.extract(r'S(\d+)[a-zA-Z]')
+df['qubits'] = df['qubits'].astype(int)
+df['steps'] = df['steps'].astype(int)
 
-df = pd.DataFrame(processed_rows, columns=column_names)
 
-print(df)
+def time_to_seconds(time_str):
+    days = 0
+    hours = 0
+    minutes = 0
+    seconds = 0
+
+    if '-' in time_str:
+        days, time_str = time_str.split('-')
+        days = int(days)
+
+
+    time_str = time_str.split('.')[0][:-3]  
+    time_parts = time_str.split(':')
+
+
+    print(time_parts)
+
+    if len(time_parts) == 1:
+        seconds = int(time_parts)
+
+    elif len(time_parts)== 2:
+        minutes = int(time_parts[0])
+        seconds = int(time_parts[1])
+
+    else:
+        hours = int(time_parts[0])
+        minutes = int(time_parts[1])
+        seconds = int(time_parts[2])
+
+    total_seconds = (days * 86400) + (hours * 3600) + (minutes * 60) + seconds
+    return int(total_seconds)
+
+df['TotalCPU_seconds'] = df['TotalCPU'].apply(time_to_seconds)
+
+def ploter(xaxis, yaxis, dataframe=df, ifqubits=6, ifsteps=2**5):
+    if (xaxis not in ('qubits', 'steps', 'AllocCPUS')):
+        raise ValueError("Cannot meet conditions for X axis")
+
+    if(yaxis not in ('steps', 'TotalCPU', ' MaxRSS')):
+        raise ValueError("Cannot meet conditions for Y axis")
+
+    if yaxis=='TotalCPU':
+        yaxis = time_to_seconds(yaxis)
+
+    df.plot(x=xaxis,y=yaxis)
+    plt.show()
+
+ploter('qubits', 'TotalCPU')
