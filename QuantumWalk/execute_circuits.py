d374b4584575bf4134991cc58a9cf2cf9b455069
Diogo Goncalves <dgoncalves@login004.lca.uc.pt>
2023-07-13 14:16:26 +0100

    as

--- QuantumWalk/execute_circuits.py
+++ QuantumWalk/execute_circuits.py
@@ -73,15 +73,15 @@ def execute_circuits(circuits_list,shots,simulator,num_threads,parallel_exp,hard
     #print(f"Run Circuit: {time.time()}")
     #if (simulator=='aer_simulator_statevector'):
     print(f"Transpile: {time.time()-start_time}")
-    circuits_list = transpile(circuits_list, backend, optimization_level=0)
+    circuits_list = transpile(circuits_list, backend, optimization_level=1)
 
     print(f"Run: {time.time()-start_time}")
     job_statevector = backend.run(circuits_list, shots=shots)
     
-    #print(f"Backend options: {backend.options}")
     print(f".result(): {time.time()-start_time}")
     answer = job_statevector.result().get_counts()
     
+    print(f"Backend options: {backend.options}")
     #else:
     #    job = execute(circuits_list, backend=backend, shots=shots)
     #    answer = job.result().get_counts()
--- submit__cache.sh
+++ submit__cache.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # set the partition where the job will run (default = normal)
-#SBATCH --partition=cpu2
+#SBATCH --partition=hmem1
 #SBATCH -A cquant
 
 # set the number of nodes and processes per node
@@ -9,10 +9,10 @@
 #SBATCH --ntasks=1
 
 # set name of job
-#SBATCH --job-name=Q06S100P80DsCU00MJS100
+#SBATCH --job-name=Q06S100P01DsCU00MJS100
 
 # set the mem for the whole job
-#SBATCH --mem-per-cpu=1100M
+#SBATCH --mem-per-cpu=4700M
 
 # set the number of tasks (processes) per node.
 #SBATCH --cpus-per-task=80
@@ -45,5 +45,5 @@ echo "number of tasks = $SLURM_NTASKS"
 echo "number of cpus_per_task = $SLURM_CPUS_PER_TASK"
 
 # Run the command
-srun mprof run python3 ./QuantumWalk/main.py 6 10 1 2 1 2 10000 statevector ${SLURM_JOB_ID} 80 CPU double 80 0 1 None
+srun mprof run python3 /veracruz/projects/c/cquant/Dirac-Quantum-Walk/QuantumWalk/main.py 6 100 1 2 1 2 10000 statevector ${SLURM_JOB_ID} 80 CPU double 1 0 1 None
 
