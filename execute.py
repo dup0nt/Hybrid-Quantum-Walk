ef510610b528ecdfca1e8b3ceeeecb3ef100c394
Diogo Goncalves <dgoncalves@login004.lca.uc.pt>
2023-07-20 09:41:06 +0100

    uPDATE

--- execute.py
+++ execute.py
@@ -10,7 +10,7 @@ precisions = ['double', 'single']
 simulators = ['statevector']#['aer_simulator_statevector','aer_simulator']
 
 
-partition = partitions[2]
+partition = partitions[4]
 precision = precisions[0]
 simulator = simulators[0]
 parallel_exps = [1]
@@ -159,7 +159,62 @@ for parallel_exp in parallel_exps:
                                                 
 
 #SBATCH --exclusive
-                        bash_execute = """#!/bin/bash
+                        if partition=='gpu':
+                            
+                            bash_execute = """#!/bin/bash
+# set the partition where the job will run (default = normal)
+#SBATCH --partition={}
+#SBATCH -A cquant
+
+# set the number of nodes and processes per node
+#SBATCH --gres=gpu:1
+
+#SBATCH --ntasks=1
+
+# set name of job
+#SBATCH --job-name={}
+
+# set the mem for the whole job
+#SBATCH --mem-per-cpu={}
+
+# set the number of tasks (processes) per node.
+#SBATCH --cpus-per-task={}
+
+
+
+# set max wallclock time (in this case 2800 minutes)
+#SBATCH --time=2800:00
+
+# out
+#SBATCH --output=/veracruz/projects/c/cquant/Dirac-Quantum-Walk/Output/FilesOut/%j.out
+
+# err and out job files
+#SBATCH --error=/veracruz/projects/c/cquant/Dirac-Quantum-Walk/Output/FilesErr/%j.err
+
+# Get the Slurm Job ID
+JOB_ID=$SLURM_JOB_ID
+
+# Use OpenMP and set environment variables
+export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
+export SLURM_JOB_ID=$SLURM_JOB_ID
+export OMPI_MCA_btl="^openib"
+export OMPI_MCA_mtl="ofi"
+
+module load Anaconda
+source activate cquant_env
+
+# Print the number of tasks per node
+echo "number of tasks = $SLURM_NTASKS"
+echo "number of cpus_per_task = $SLURM_CPUS_PER_TASK"
+
+# Run the command
+srun mprof run --output /veracruz/projects/c/cquant/Dirac-Quantum-Walk/Output/Profiler/Data/${{SLURM_JOB_ID}}.prof python3 /veracruz/projects/c/cquant/Dirac-Quantum-Walk/QuantumWalk/main.py {} {} {} {} {} {} {} {} ${{SLURM_JOB_ID}} {} {} {} {} {} {} {} {}
+
+""".format(partition,job_name, partitions_details[partition]['memory'],thread,qubit,step,coin_type,theta,boundary,dist_boundary,shots,simulator,thread,hardware,precision,parallel_exp,batching,multiple_circuits, job_size,split_circuits_per_cluster_node)
+
+                        else:
+
+                            bash_execute = """#!/bin/bash
 # set the partition where the job will run (default = normal)
 #SBATCH --partition={}
 #SBATCH -A cquant
